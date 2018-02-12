f1400fdcdacfd4828a57d7de5fab18be9acdee8e
Noah Clark <noah@gamersensei.com>
2018-02-12 08:36:58 -0500

    Initial Commit

---
+++ Bip The GuyTests/Bip_The_GuyTests.swift
@@ -0,0 +1,36 @@
+//
+//  Bip_The_GuyTests.swift
+//  Bip The GuyTests
+//
+//  Created by Noah Clark on 2/12/18.
+//  Copyright © 2018 Noah Clark. All rights reserved.
+//
+
+import XCTest
+@testable import Bip_The_Guy
+
+class Bip_The_GuyTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
